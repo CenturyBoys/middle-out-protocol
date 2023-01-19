8b184f44276a895ebd958cd38a252ed496886004
Igor Magro <iggormagro8@gmail.com>
2023-01-18 23:50:41 -0300

    refactor: make a request args a model

--- src/domain/websocket/request/model.py
+++ src/domain/websocket/request/model.py
@@ -1,33 +1,23 @@
 from dataclasses import dataclass
 
-from src.domain.exception.server.model import InvalidRequestArgsException
+from src.domain.websocket.request.args.model import Args
 from src.domain.websocket.request.method.enum import Method
 
 
 @dataclass(slots=True)
 class Request:
     __id: str
-    __args: dict
+    __args: Args
     __header: dict
     __route: str
     __method: Method
 
     def __init__(self, id: str, args: dict, header: dict, route: str, method: str):
         self.__id = id
-        self.__args = self.validate_args(args=args)
+        self.__args = Args.create(args=args)
         self.__header = header
         self.__route = route
         self.__method = Method.validate_method(method=method)
-
-    @staticmethod
-    def validate_args(args):
-        is_dict_args = type(args) == dict
-        if not is_dict_args:
-            raise InvalidRequestArgsException(
-                f"Request args {args} is invalid"
-            )
-
-        return args
 
     @property
     def id(self):
@@ -35,7 +25,7 @@ class Request:
 
     @property
     def args(self):
-        return self.__args
+        return self.__args.args
 
     @property
     def header(self):
